c13c1ddddc99e08d522c7d7d22d5bf29a323f22c
Unknown <edanuramail@gmail.com>
2020-06-30 23:34:17 -0400

    Person added to grief scene

--- Assets/Scripts/Grief/StarFall.cs
+++ Assets/Scripts/Grief/StarFall.cs
@@ -2,29 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class StarFall : MonoBehaviour
 {
     private Rigidbody2D rb;
 
+    public GameObject person;
+
     private Darker _darker;
     private SkyManager _skyManager;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
 
-        _skyManager = FindObjectOfType<SkyManager>();
+        Collider2D col1 = GetComponent<Collider2D>();
+        Collider2D col2 = person.GetComponent<Collider2D>();
         
+        Debug.Log(col2);
+
+        if (col1.IsTouching(col2))
+        {
+            Debug.Log("touching now");
+            Vector3 screenSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+            Vector3 spawnPos = new Vector3(Random.Range(-screenSize.x, screenSize.x) * .9f,
+                Random.Range(-screenSize.y * .7f, screenSize.y) * .9f, 0);
+            transform.position = spawnPos;
+        }
+
+        _skyManager = FindObjectOfType<SkyManager>();
+
         // the image that gets darker as we lose stars
         _darker = FindObjectOfType<Darker>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private bool touched = false;
     private void OnTriggerEnter2D(Collider2D other)
