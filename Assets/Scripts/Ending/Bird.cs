76e6e1a650f35bbc22f46acfccfa8812264d4031
Unknown <edanuramail@gmail.com>
2020-07-01 23:09:31 -0400

    Ending scene with bird working, credits added

---
+++ Assets/Scripts/Ending/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Animator _animator;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("end"))
+        {
+            Services.GameManager.nextScene = true;
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _animator.SetBool("isFlying", true);
+        }
+    }
+}
