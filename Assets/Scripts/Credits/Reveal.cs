6fdbebf0b641826c302763d867dcd2049f134aeb
Unknown <edanuramail@gmail.com>
2020-07-01 23:19:21 -0400

    Minor changes

---
+++ Assets/Scripts/Credits/Reveal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.LWRP;
+using UnityEngine.Rendering.Universal;
+
+public class Reveal : MonoBehaviour
+{
+    private Bloom _bloom;
+    void Start()
+    {
+        _bloom = GetComponent<Bloom>();
+        _bloom.threshold =new MinFloatParameter(.1f, .1f, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
