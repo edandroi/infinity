6cf0786e70716b84f6a32bae837b7a008903721b
Unknown <edanuramail@gmail.com>
2020-07-13 22:40:10 -0400

    Get up interaction changed

--- Assets/Scripts/GetUp/GetUp.cs
+++ Assets/Scripts/GetUp/GetUp.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Timers;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class GetUp : MonoBehaviour
 {
     public Sprite[] sprites;
     private SpriteRenderer _spriteRenderer;
+
+    private Collider2D col;
 
     private Player _player;
 
@@ -16,6 +20,7 @@ public class GetUp : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _spriteRenderer.sprite = sprites[0];
         _player = FindObjectOfType<Player>();
+        col = GetComponent<Collider2D>();
         
         screenSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
     }
@@ -32,8 +37,8 @@ public class GetUp : MonoBehaviour
 
     void Update()
     {
-//        Debug.Log(_player.transform.position.y);
-        MousePos();
+//        MousePos();
+
         if (spriteNum == sprites.Length - 1) // if we reach the final sprite
         {
             Services.GameManager.nextScene = true;
@@ -41,12 +46,23 @@ public class GetUp : MonoBehaviour
     }
 
     public int spriteNum = 0;
-  
+
+    private Vector3 m_Size;
+    private Bounds _bounds;
     public void ChangeSprite()
     {
         spriteNum++;
         _spriteRenderer.sprite = sprites[spriteNum];
     }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("now");
+        if (other.gameObject.CompareTag("Player"))
+        ChangeSprite();
+    }
+
 
     private bool started = false;
     void MousePos()
